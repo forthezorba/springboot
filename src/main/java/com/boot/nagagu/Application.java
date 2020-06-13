74ac1f99fd51bfbee94af00ec0a9a4c1cfb1e2b3
zorbaKT <forthezorba@gmail.com>
2020-06-13 19:19:04 +0900

    Oauth2 google,naver

--- src/main/java/com/boot/nagagu/Application.java
+++ src/main/java/com/boot/nagagu/Application.java
@@ -2,9 +2,8 @@ package com.boot.nagagu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing 분리 -> config
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
