dac0c76a213802dcd7c3692325df9e4b9a7db1fd
zorbaKT <forthezorba@gmail.com>
2020-06-13 16:41:16 +0900

    Oauth

---
+++ src/main/java/com/boot/nagagu/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,32 @@
+package com.boot.nagagu.config.auth;
+
+import com.boot.nagagu.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    //@LoginUser어노테이션이 붙어있고, 파라미터 클래스타입=sessionuser.class인지 확인
+    @Override
+    public boolean supportsParameter(MethodParameter parameter){
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+    //파라미터에 전달할 객체 생성(세션에서 객체 가져옴)
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception{
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/boot/nagagu/web/IndexController.java
+++ src/main/java/com/boot/nagagu/web/IndexController.java
@@ -1,18 +1,30 @@
 package com.boot.nagagu.web;
 
+import com.boot.nagagu.config.auth.LoginUser;
+import com.boot.nagagu.config.auth.dto.SessionUser;
 import com.boot.nagagu.service.PostsService;
+import com.boot.nagagu.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user!=null){
+            model.addAttribute("username",user.getName());
+        }
         return "index";
     }
 
@@ -20,4 +32,13 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
+    }
+
+
 }
